1680646d88b025164a7911730e2699df737694f8
Robyn Harka <reharka@gmail.com>
2018-03-20 14:26:08 -0400

    domain

--- src/Auth/auth0-variables.js
+++ src/Auth/auth0-variables.js
@@ -1,5 +1,5 @@
 export const AUTH_CONFIG = {
-  domain: 'budgetboss.auth0.com',
-  clientId: 'oo2HSSzBwlcQ5rHumPEGseWP_hyKcJFs',
+  domain: 'app91318666.auth0.com',
+  clientId: '-mUe9JwKmXPdeuMyXiDIbvr36ZGV3xQc',
   callbackUrl: 'http://localhost:3000/callback' || 'https://cryptic-spire-28248.herokuapp.com/callback'
 }
