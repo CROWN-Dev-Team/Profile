33ca38c7a09ee98af9c4a1a259fc114dea580b7c
Robyn Harka <reharka@gmail.com>
2018-03-20 14:28:23 -0400

    callback

--- src/Auth/auth0-variables.js
+++ src/Auth/auth0-variables.js
@@ -1,5 +1,5 @@
 export const AUTH_CONFIG = {
   domain: 'app91318666.auth0.com',
   clientId: '-mUe9JwKmXPdeuMyXiDIbvr36ZGV3xQc',
-  callbackUrl: 'http://localhost:3000/callback' || 'https://cryptic-spire-28248.herokuapp.com/callback'
+  callbackUrl: 'https://cryptic-spire-28248.herokuapp.com/callback'
 }
