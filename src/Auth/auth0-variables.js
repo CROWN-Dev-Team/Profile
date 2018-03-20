2a6c1cd7cb8b1c2939cde8d4b094681653015aab
Robyn Harka <reharka@gmail.com>
2018-03-20 14:24:28 -0400

    heroku

--- src/Auth/auth0-variables.js
+++ src/Auth/auth0-variables.js
@@ -1,5 +1,5 @@
 export const AUTH_CONFIG = {
   domain: 'budgetboss.auth0.com',
   clientId: 'oo2HSSzBwlcQ5rHumPEGseWP_hyKcJFs',
-  callbackUrl: 'http://localhost:3000/callback'
+  callbackUrl: 'http://localhost:3000/callback' || 'https://cryptic-spire-28248.herokuapp.com/callback'
 }
